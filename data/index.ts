89a834bfeead91b56bd76f08ae8d6b017040f7b1
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-06-24 17:12:40 -0500

    added new testimonial

--- data/index.ts
+++ data/index.ts
@@ -175,6 +175,12 @@ export const testimonials = [
     name: "Matthew Allen",
     title: "MeadTools User, Software Engineer",
   },
+  {
+    quote:
+      "MeadTools, developed by Larry Reaux, is a comprehensive tool that has become indispensable for my mead-making process, providing everything I need from recipe planning and seamless unit conversion (metric <-> US) to a suite of calculators that would otherwise be scattered across the internet. I followed the project's development and was consistently impressed by Larry Reaux' pragmatic problem-solving skills and commitment to user feedback. Their friendly and responsive communication made providing feedback enjoyable, and I appreciated their ability to quickly address issues. Larry Reaux' adaptability and efficiency would make them a valuable asset to any development team.",
+    name: "Drews C.",
+    title: "Senior Android Developer, Meadtools German Translator",
+  },
 ];
 export const workExperience = [
   {
